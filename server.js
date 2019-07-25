27c41888dd84f80eb602d4e308eead162a3464b5
Vu Linh <vlinh12300@gmail.com>
2019-07-25 14:05:47 +0700

    Giai quyet rac roi

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get("/contact",(req,res) => {
     res.send("<p>Contact here</p>");
 });
 
+app.get('/about',(req,res) => {
+    res.send("<p>What about me ?</p>")
+});
+
 app.listen(port,hostname,() => {
     console.log(`Server is running at => ${hostname}:${port}`);
 });
