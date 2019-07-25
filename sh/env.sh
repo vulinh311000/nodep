1cf6a59a45858dcf492e7097c4f555a87f1767a6
Vu Linh <vlinh12300@gmail.com>
2019-07-25 15:56:46 +0700

    Add item to mongodb

---
+++ sh/env.sh
@@ -0,0 +1,7 @@
+# Config database environment variable
+export DB_CONNECTION=mongodb
+export DB_HOST=localhost
+export DB_PORT=27017
+export DB_NAME=chat
+export DB_USERNAME=""
+export DB_PASSWORD=""
