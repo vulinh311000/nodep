601f4e3d60f4c4dd5d9bd5d99b226d2a61f97aa5
Vu Linh <vlinh12300@gmail.com>
2019-07-26 13:08:03 +0700

    [1.0.0] Register user part 01.

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,22 @@
+import {validationResult} from 'express-validator/check';
+
 export const getLoginRegister = (req, res) => {
-  return res.render("auth/master");
+    return res.render("auth/master");
 };
 
 export const getLogout = (req, res) => {
-  return res.send("Log out");
+    return res.send("Log out");
+};
+
+export const postRegister = (req, res) => {
+    const errorArr = [];
+    const validationErrors = validationResult(req);
+    if(!validationErrors.isEmpty()) {
+        const errors = Object.values(validationErrors.mapped());
+        errors.forEach(elm => {
+            errorArr.push(elm.msg);
+        });
+        console.log(errorArr);
+    }
+    console.log(req.body);
 };
